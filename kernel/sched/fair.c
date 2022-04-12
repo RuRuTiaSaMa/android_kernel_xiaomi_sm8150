MotorolaMobilityLLC/kernel-msm

sched/fair: Improve the scheduler
This change is for general scheduler improvement.

Change-Id: I7a84c7f7444d57f31e0859c4d39450d23421eb73
Signed-off-by: Pavankumar Kondeti <pkondeti@codeaurora.org>

kernel/sched/fair.c
@@ -6910,7 +6910,8 @@ static void find_best_target(struct sched_domain *sd, cpumask_t *cpus,
 		goto out;
 
 	/* fast path for prev_cpu */
-	if ((capacity_orig_of(prev_cpu) == capacity_orig_of(start_cpu)) &&
+	if (((capacity_orig_of(prev_cpu) == capacity_orig_of(start_cpu)) ||
+		asym_cap_siblings(prev_cpu, start_cpu)) &&
 		!cpu_isolated(prev_cpu) && cpu_online(prev_cpu) &&
 		idle_cpu(prev_cpu)) {
 

RuRuTiaSaMa/android_kernel_xiaomi_sm8150

Signed-off-by: RuRuTiaSaMa <1009087450@qq.com>

kernel/sched/fair.c
@@ -7605,7 +7605,8 @@ static inline int find_best_target(struct task_struct *p, int *backup_cpu,
 		return -1;
 
 	/* fast path for prev_cpu */
-	if ((capacity_orig_of(prev_cpu) == capacity_orig_of(cpu)) &&
+	if (((capacity_orig_of(prev_cpu) == capacity_orig_of(cpu)) ||
+		asym_cap_siblings(prev_cpu, cpu)) &&
 		!cpu_isolated(prev_cpu) && cpu_online(prev_cpu) &&
 		idle_cpu(prev_cpu)) {
 